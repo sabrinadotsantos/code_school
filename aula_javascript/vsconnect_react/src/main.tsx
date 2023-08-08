5eb56dc149ba406c78158eab86e3e9cb8ae3e41b
Sabrina Santos <sabrinadotsantos@gmail.com>
2023-08-07 21:55:20 -0300

    vs connect com react

---
+++ aula_javascript/vsconnect_react/src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//componentes
+import Home from './pages/Home/index';
+
+//estilizaçào global
+
+import "./index.css";
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+)
