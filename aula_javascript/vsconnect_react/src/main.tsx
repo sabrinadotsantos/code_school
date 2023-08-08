8f6a6250bc8ef588fec6d56dc857a05db53dec24
Sabrina Santos <sabrinadotsantos@gmail.com>
2023-08-08 20:00:46 -0300

    Atividade - Importar a tela de serviços no projeto vsconnect em React

--- aula_javascript/vsconnect_react/src/main.tsx
+++ aula_javascript/vsconnect_react/src/main.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 //componentes
-import Home from './pages/Home/index';
+// import Home from './pages/Home/index';
+import ListaServicos from './pages/ListaServicos/index';
 
 //estilizaçào global
 
-import "./index.css";
-
+import "./index.css" // css padrão para todas as paginas
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    <ListaServicos/>
   </React.StrictMode>,
 )
