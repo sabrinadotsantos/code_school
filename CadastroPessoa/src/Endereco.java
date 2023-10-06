ce798ded972d4fa087b082ee1825a7bccc532452
Sabrina Santos <sabrinadotsantos@gmail.com>
2023-10-05 21:42:34 -0300

    introduçào a poo

--- CadastroPessoa/src/Endereco.java
+++ CadastroPessoa/src/Endereco.java
@@ -1,2 +1,5 @@
 public class Endereco {
+    public String logradouro;
+    public int numero;
+    public boolean endComercial;
 }
--- CadastroPessoa/src/Pessoa.java
+++ CadastroPessoa/src/Pessoa.java
@@ -1,2 +1,9 @@
 public class Pessoa {
+    public String nome;
+    public float rendimento;
+    public Endereco endereco; //chamando a classe Endereco
+
+    public float CalcularImposto(float rendimento) {
+        return rendimento;
+    }
 }
