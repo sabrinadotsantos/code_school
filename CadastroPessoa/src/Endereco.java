710a581fef1d35a9812e3ffc41b8d56a357646c0
Sabrina Santos <sabrinadotsantos@gmail.com>
2023-10-20 20:01:09 -0300

    criação de scripts banco de dados vsconnect

--- CadastroPessoa/src/Endereco.java
+++
@@ -1,5 +0,0 @@
-public class Endereco {
-    public String logradouro;
-    public int numero;
-    public boolean endComercial;
-}
--- CadastroPessoa/src/Pessoa.java
+++
@@ -1,9 +0,0 @@
-public class Pessoa {
-    public String nome;
-    public float rendimento;
-    public Endereco endereco; //chamando a classe Endereco
-
-    public float CalcularImposto(float rendimento) {
-        return rendimento;
-    }
-}
