ef0adfbf5ea71d94f6889a7de369e6129cce7539
Sabrina Santos <sabrinadotsantos@gmail.com>
2023-10-24 21:50:22 -0300

    inicio api vsconnect

--- apivsconnect/src/main/java/com/senai/apivsconnect/ApivsconnectApplication.java
+++ apivsconnect/src/main/java/com/senai/apivsconnect/ApivsconnectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApivsconnectApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApivsconnectApplication.class, args);
 	}
