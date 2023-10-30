33b4f1f3b5f1f124c38b287b4af1c5dcc8d22fcb
Sabrina Santos <sabrinadotsantos@gmail.com>
2023-10-30 19:50:30 -0300

    inicio atividade desenvolvimento api volksway

---
+++ apivolksway/src/main/java/com/senai/apivolksway/ApivolkswayApplication.java
@@ -0,0 +1,13 @@
+package com.senai.apivolksway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApivolkswayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApivolkswayApplication.class, args);
+	}
+
+}
