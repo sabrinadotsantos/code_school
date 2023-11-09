b783583e277b7c049d93f749d44aeb583ef41b89
Sabrina Santos <sabrinadotsantos@gmail.com>
2023-11-09 20:32:53 -0300

    atualizacao de scripts

--- scritpts_volksway/DDL_volksway.sql
+++ scritpts_volksway/DDL_volksway.sql
@@ -2,69 +2,82 @@ CREATE DATABASE bd_volksway;
 
 USE bd_volksway;
 
+DROP TABLE tipo_usuario;
+
 
 CREATE TABLE tb_empresas(
 	id_empresa BINARY(16) NOT NULL,
-	razaoSocial VARCHAR(50) NOT NULL,
+	razao_social VARCHAR(50) NOT NULL,
     cidade VARCHAR(50) NOT NULL,
     cnpj CHAR(18) NOT NULL UNIQUE,
     PRIMARY KEY(id_empresa)
 );
 
+DROP database bd_volksway;
+
+DROP TABLE tb_usuarios;
+
 CREATE TABLE tb_usuarios(
 	id_usuario BINARY(16) NOT NULL,
+    id_empresa BINARY(16) NOT NULL,
     telefone VARCHAR (15),
     email VARCHAR (50) NOT NULL UNIQUE,
     senha VARCHAR (50) NOT NULL,
-    dataNascimento DATE NOT NULL,
+    data_nascimento DATE NOT NULL,
     cidade VARCHAR (50) NOT NULL,
     cpf CHAR(14) NOT NULL,
-    tipoUsuario TINYINT NOT NULL,
+    tipo_usuario TINYINT NOT NULL,
     nome VARCHAR(50) NOT NULL,
     PRIMARY KEY(id_usuario),
     FOREIGN KEY(id_empresa) REFERENCES tb_empresas(id_empresa)
 );
 
 
-
 CREATE TABLE tb_veiculos(
 	id_veiculo BINARY(16) NOT NULL,
-	codigoChassi CHAR(20) NOT NULL,
+    id_usuario BINARY(16) NOT NULL,
+	codigo_chassi CHAR(20) NOT NULL,
     marca VARCHAR(30) NOT NULL,
     placa CHAR(8) NOT NULL,
     PRIMARY KEY(id_veiculo),
     FOREIGN KEY(id_usuario) REFERENCES tb_usuarios(id_usuario)
-); 
+);
 
 CREATE TABLE tb_checklists(
 	id_checklist BINARY(16) NOT NULL,
+    id_veiculo BINARY(16) NOT NULL,
 	freio BIT(1) NOT NULL,
     combustivel BIT(1) NOT NULL,
     oleo BIT(1) NOT NULL,
-    fotoPneu VARCHAR(255) NOT NULL,
-    arCondicionado BIT(1) NOT NULL,
+    foto_pneu VARCHAR(255) NOT NULL,
+    ar_condicionado BIT(1) NOT NULL,
     PRIMARY KEY(id_checklist),
     FOREIGN KEY(id_veiculo) REFERENCES tb_veiculos(id_veiculo)
 );
-     
+
+DROP TABLE tb_propagandas;
+
 CREATE TABLE tb_propagandas(
 id_propaganda BINARY(16) NOT NULL,
+id_usuario BINARY(16) NOT NULL,
 titulo VARCHAR(50) NOT NULL,
 url VARCHAR(255) NOT NULL,
 descricao VARCHAR(255) NOT NULL,
 img VARCHAR(255) NOT NULL,
-publicoAlvo VARCHAR(30) NOT NULL,
-dataLimite DATE NOT NULL,
+publico_alvo VARCHAR(30) NOT NULL,
+data_limite DATE NOT NULL,
 preco FLOAT(10, 2),
 importancia BIT(1) NOT NULL,
-nomeTipoPropaganda VARCHAR(20) NOT NULL,
+nome_tipo_propaganda VARCHAR(20) NOT NULL,
 PRIMARY KEY(id_propaganda),
-FOREIGN KEY(id_usuario) REFERENCES tb_usuarios(id_usuario) 
+FOREIGN KEY(id_usuario) REFERENCES tb_usuarios(id_usuario)
 );
 
 CREATE TABLE tb_interesses(
 	id_interesse BINARY(16) NOT NULL,
-    meioContatoEmail BIT(1),
+    id_propaganda BINARY(16) NOT NULL,
+    id_usuario BINARY(16) NOT NULL,
+    meio_contato_email BIT(1),
     PRIMARY KEY(id_interesse),
     FOREIGN KEY(id_propaganda) REFERENCES tb_propagandas(id_propaganda),
     FOREIGN KEY(id_usuario) REFERENCES tb_usuarios(id_usuario)
--- scritpts_volksway/DML_volksway.sql
+++ scritpts_volksway/DML_volksway.sql
@@ -1,30 +1,47 @@
 USE bd_volksway;
 
+INSERT INTO tb_empresas VALUES (
+	(UUID_TO_BIN(UUID())),
+    "Caminhoes LTDA",
+    "Rio de Janeiro",
+    "123456789098765432"
+);
+
+INSERT INTO tb_empresas VALUES (
+	(UUID_TO_BIN(UUID())),
+    "Volks LTDA",
+    "Bahia",
+    "12346789098765432"
+);
+
+SELECT * FROM tb_empresas;
+SELECT BIN_TO_UUID(id_empresa), razao_social FROM tb_empresas;
+
 INSERT INTO tb_usuarios VALUES (
-	(UUID_TO_BIN(UUID())), 
+	(UUID_TO_BIN(UUID())),
+    UUID_TO_BIN('46ae3cba-7910-11ee-bfa0-b445067b827e'),
     "1155554444",
     "sabrina@email.com",
     "senai",
-    "1997/19/12",
+    "1997-12-19",
     "Sao Paulo",
     00000000000,
     1,
     "Sabrina Santos da Silva"
 );
 
-INSERT INTO tb_empresas VALUES (
-	(UUID_TO_BIN(UUID())),
-    "Caminhoes LTDA",
-    "Rio de Janeiro"
-    "123456789098765432"
-);
+SELECT * FROM tb_usuarios;
+SELECT BIN_TO_UUID(id_usuario), nome FROM tb_usuarios;
 
 INSERT INTO tb_veiculos VALUES (
 	(UUID_TO_BIN(UUID())),
-    "12345678909876543210"
+    UUID_TO_BIN('54a24d8d-7910-11ee-bfa0-b445067b827e'),
+    "1234567899876543210"
     "Volkswagen",
     "ABC445K9"
 );
+
+SELECT * FROM tb_veiculos;
 
 INSERT INTO tb_checklists VALUES (
 	(UUID_TO_BIN(UUID())),
@@ -48,17 +65,9 @@ INSERT INTO tb_propagandas VALUES (
     "Banner"
 );
 
-INSERT INTO tb_propagandas VALUES (
+INSERT INTO tb_interesses VALUES (
 	(UUID_TO_BIN(UUID())),
-    "Propaganda Teste",
-	"teste teste",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
-    "img teste",
-    "motorista",
-    "2024/23/10"
-    "",
-    1,
-    "Banner"
+	1
 );
 
 
