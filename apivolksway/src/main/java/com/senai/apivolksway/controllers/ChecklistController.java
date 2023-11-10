154435a435c966b3ad040dd26bb7c9a61d310fa3
Sabrina Santos <sabrinadotsantos@gmail.com>
2023-11-09 21:46:06 -0300

    continuaçao apivolksway

--- apivolksway/src/main/java/com/senai/apivolksway/controllers/ChecklistController.java
+++ apivolksway/src/main/java/com/senai/apivolksway/controllers/ChecklistController.java
@@ -5,7 +5,7 @@ import com.senai.apivolksway.dtos.ChecklistDto;
 import com.senai.apivolksway.models.ChecklistModel;
 import com.senai.apivolksway.repositories.ChecklistRepository;
 import jakarta.validation.Valid;
-import org.apache.tomcat.util.http.fileupload.FileUpload;
+import com.senai.apivolksway.services.FileUploadService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,7 +26,7 @@ public class ChecklistController {
     ChecklistRepository checklistRepository;
 
     @Autowired
-    MultipartFile fileUploadService;
+    FileUploadService fileUploadService;
 
     //Get
     @GetMapping
@@ -52,18 +52,57 @@ public class ChecklistController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Checklist já cadastrado!");
         }
 
-       ChecklistModel novoChecklist = new ChecklistModel();
+        ChecklistModel novoChecklist = new ChecklistModel();
         BeanUtils.copyProperties(checklistDto, novoChecklist);
 
         String urlImagem;
+
         try {
             urlImagem = fileUploadService.fazerUpload(checklistDto.imagem());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-//        novoChecklist.setUrl_img(urlImagem);
+        novoChecklist.setFoto_pneu(urlImagem);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(checklistRepository.save(novoChecklist));
+
+    }
+
+    //Put
+    @PutMapping(value = "/idChecklist", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<Object> editarChecklist(@PathVariable(value = "idChecklist") UUID id, @ModelAttribute @Valid ChecklistDto checklistDto) {
+        Optional<ChecklistModel> checklistBuscada = checklistRepository.findById(id);
+
+        if (checklistBuscada.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Checklist não encontrada");
+        }
+        ChecklistModel checklistBd = checklistBuscada.get();
+        BeanUtils.copyProperties(checklistDto, checklistBd);
+
+        String urlImagem;
+
+        try {
+            urlImagem = fileUploadService.fazerUpload(checklistDto.imagem());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        checklistBd.setFoto_pneu(urlImagem);
+
+        return ResponseEntity.status(HttpStatus.OK).body(checklistRepository.save(checklistBd));
+    }
+
+    //Delete
+    @DeleteMapping("/{idChecklist}")
+    public ResponseEntity<Object> deletarChecklist(@PathVariable(value = "idChecklist") UUID id) {
+        Optional<ChecklistModel> checklistBuscada = checklistRepository.findById(id);
+
+        if (checklistBuscada.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado");
+        }
+
+        checklistRepository.delete(checklistBuscada.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Usuario deletado com sucesso!");
     }
 }
--- apivolksway/src/main/java/com/senai/apivolksway/dtos/ChecklistDto.java
+++ apivolksway/src/main/java/com/senai/apivolksway/dtos/ChecklistDto.java
@@ -19,6 +19,6 @@ public record ChecklistDto(
 
         @NotBlank Boolean oleo,
 
-        @NotBlank MultipartFile imagem
+        MultipartFile imagem
         ) {
 }
