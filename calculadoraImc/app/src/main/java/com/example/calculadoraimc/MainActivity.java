cc2aab82a86bf5a44a4b8a37ef8f256c1799e200
Sabrina Santos <sabrinadotsantos@gmail.com>
2024-01-12 21:52:29 -0300

    atividade calculadora imc

--- calculadoraImc/app/src/main/java/com/example/calculadoraimc/MainActivity.java
+++ calculadoraImc/app/src/main/java/com/example/calculadoraimc/MainActivity.java
@@ -1,17 +1,14 @@
 package com.example.calculadoraimc;
 
 import android.os.Bundle;
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 import com.example.calculadoraimc.databinding.ActivityMainBinding;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +22,53 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+        TextView resultado = findViewById(R.id.resultado);
+
+        Button botaoCalcular = findViewById(R.id.botaoCalcular);
+        botaoCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                calcular();
+            }
+            private void calcular (){
+
+                TextView altura = findViewById(R.id.altura);
+                TextView peso = findViewById(R.id.peso);
+                TextView classificao = findViewById(R.id.classificacao);
+
+                double alturaNova;
+                double pesoNovo;
+                double imcCalculado;
+
+                 alturaNova = Double.parseDouble(altura.getText().toString());
+                 pesoNovo = Double.parseDouble(peso.getText().toString());
+
+                 imcCalculado = pesoNovo / (alturaNova * alturaNova);
+
+                String imcFormatado = String.format("%.2f", imcCalculado);
+
+
+                resultado.setText(imcFormatado);
+
+
+                if (imcCalculado < 18.5) {
+                    classificao.setText("Filezinho!");
+
+                } else if (imcCalculado < 25) {
+                   classificao.setText("Diliça!");
+                } else if (imcCalculado < 30) {
+                    classificao.setText("Ta top!");
+                } else {
+                    classificao.setText("O la em casa!");
+                }
+
+                classificao.setVisibility(View.VISIBLE);
+                resultado.setVisibility(View.VISIBLE);
+
+            }
+        });
 
     }
 }
