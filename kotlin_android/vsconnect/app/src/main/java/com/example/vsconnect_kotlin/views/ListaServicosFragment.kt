fe712e6b8d85b3338d1aaafa5cfef48ac8d6e8bd
Sabrina Santos <sabrinadotsantos@gmail.com>
2024-01-16 19:19:49 -0300

    inicio kotlin android

---
+++ kotlin_android/vsconnect/app/src/main/java/com/example/vsconnect_kotlin/views/ListaServicosFragment.kt
@@ -0,0 +1,34 @@
+package com.example.vsconnect_kotlin.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.vsconnect_kotlin.databinding.FragmentGalleryBinding
+
+class ListaServicosFragment : Fragment() {
+
+    private var _binding: FragmentGalleryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
