9f0e858e6e94234c2742afb2b96daf3986bb372c
Sabrina Santos <sabrinadotsantos@gmail.com>
2024-01-17 21:52:38 -0300

    inicio construcao da tela de login

--- kotlin_android/vsconnect/app/src/main/java/com/example/vsconnect_kotlin/MainActivity.kt
+++ kotlin_android/vsconnect/app/src/main/java/com/example/vsconnect_kotlin/MainActivity.kt
@@ -29,10 +29,7 @@ class MainActivity : AppCompatActivity() {
         //desabilita a exibicao do titulo da tela atual
         supportActionBar?.setDisplayShowTitleEnabled(false);
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
