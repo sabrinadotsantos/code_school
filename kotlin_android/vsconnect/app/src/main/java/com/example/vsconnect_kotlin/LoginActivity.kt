c2786a961c283d50e383a8d91f8f9796c762cd9c
Sabrina Santos <sabrinadotsantos@gmail.com>
2024-01-19 21:54:13 -0300

    inicio da tela de servicos

--- kotlin_android/vsconnect/app/src/main/java/com/example/vsconnect_kotlin/LoginActivity.kt
+++ kotlin_android/vsconnect/app/src/main/java/com/example/vsconnect_kotlin/LoginActivity.kt
@@ -1,11 +1,29 @@
 package com.example.vsconnect_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.vsconnect_kotlin.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        binding.btnEntrar.setOnClickListener {
+
+            val mainIntent = Intent (this@LoginActivity, MainActivity::class.java)
+
+            startActivity(mainIntent)
+
+            finish()
+
+        }
+        setContentView(binding.root)
     }
+
 }
